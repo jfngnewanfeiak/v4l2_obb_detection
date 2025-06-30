22fb89b087d91b74654391325b4af337cd41c787
taishin <ok220259@oks.iput.ac.jp>
2025-06-30 19:48:25 +0900

    使う画像とbest.pt

---
+++ run_test/test.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO 
+
+def main():
+    model = YOLO("/home/user/ros2_ws/src/run_test/run_test/best.pt")
+    results = model("/home/user/ros2_ws/src/run_test/run_test/image9.png",save=True)
+
+    for result in results:
+        xywhr = result.obb.xywhr  # center-x, center-y, width, height, angle (radians)
+        xyxyxyxy = result.obb.xyxyxyxy  # polygon format with 4-points
+        names = [result.names[cls.item()] for cls in result.obb.cls.int()]  # class name of each box
+        confs = result.obb.conf  # confidence score of each box
+
+
+
+if __name__ == "__main__":
+    main()
