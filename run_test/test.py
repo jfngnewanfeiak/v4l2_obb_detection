c9126d544b49b9ed520ae147af60898e17436fb9
山本　大深 <ok220259@oks.iput.ac.jp>
2025-07-01 16:09:42 +0900

    forで繰り返すように変更

--- run_test/test.py
+++ run_test/test.py
@@ -1,15 +1,16 @@
 from ultralytics import YOLO 
-
+LOOP = 10
 def main():
     model = YOLO("/home/user/ros2_ws/src/run_test/run_test/best.pt")
-    results = model("/home/user/ros2_ws/src/run_test/run_test/image9.png",save=True)
+    for i in range(LOOP):
 
-    for result in results:
-        xywhr = result.obb.xywhr  # center-x, center-y, width, height, angle (radians)
-        xyxyxyxy = result.obb.xyxyxyxy  # polygon format with 4-points
-        names = [result.names[cls.item()] for cls in result.obb.cls.int()]  # class name of each box
-        confs = result.obb.conf  # confidence score of each box
+        results = model(f"/home/user/ros2_ws/src/run_test/run_test/image{i}.png",save=True)
 
+        for result in results:
+            xywhr = result.obb.xywhr  # center-x, center-y, width, height, angle (radians)
+            xyxyxyxy = result.obb.xyxyxyxy  # polygon format with 4-points
+            names = [result.names[cls.item()] for cls in result.obb.cls.int()]  # class name of each box
+            confs = result.obb.conf  # confidence score of each box
 
 
 if __name__ == "__main__":
